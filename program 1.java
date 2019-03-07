edcdef6ccb51daaa35295ddd7e83e8860e05abf4
AfeelAnjum12 <48013936+AfeelAnjum12@users.noreply.github.com>
2019-03-07 00:21:18 -0800

    Add files via upload

---
+++ program 1.java
@@ -0,0 +1,9 @@
+ 
+
+class program1
+{
+ public static void main(String[] args) {
+		System .out.println("Hello world  "); 
+	}
+}
+Instructions: Save the file      with class name , other wise  java not accepted
