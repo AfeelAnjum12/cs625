e0f177591d540ddf79e2d914720cc04a5ecc34ee
AfeelAnjum12 <48013936+AfeelAnjum12@users.noreply.github.com>
2019-05-08 11:41:29 -0700

    comment

--- Test2.java
+++ Test2.java
@@ -1,9 +1,12 @@
- public class Test1 {
+ public class Test2 {
     public static void main(String[] args) {
        int x=0;
 	int y=0;
 	 while(x<5){
-	   y=y+x;
+	   y=y+2;
+	   if(y>4){
+	   	y=y-1;
+	   }
 	     System.out.println(x + "" + y +" ");
 	        x=x+1;
 	   }
--- Test3.java
+++ Test3.java
@@ -1,11 +1,13 @@
- public class Test1 {
+ public class Test3 {
     public static void main(String[] args) {
        int x=0;
 	int y=0;
 	 while(x<5){
-	   y=y+x;
+	   x=x+1;
+	  y=y+x;
 	     System.out.println(x + "" + y +" ");
 	        x=x+1;
 	   }
 	}
    }
+   
